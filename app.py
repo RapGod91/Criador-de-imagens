1fbea243060176d84b4cc56396474c63e21bb4f8
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-05-08 11:23:06 -0300

    tamanho da fonte no deploy

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def create_image_with_text(text):
         draw = ImageDraw.Draw(background)
         
         # Configura a fonte (ajuste o tamanho conforme necessário)
-        font_size = 35
+        font_size = 50
         try:
             font = ImageFont.truetype("arial.ttf", font_size)
         except:
